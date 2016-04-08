2bf2cacd0c78556e8417e7bebda0fe9d5286ceef
Cerem Cem ASLAN <ceremcem@ceremcem.net>
2016-04-08 08:07:49 +0300

    make-bundle script uses modules from ./modules folder

--- make-bundle.sh
+++ make-bundle.sh
@@ -3,6 +3,7 @@
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 cd "${DIR}/app"
 
+MODULES_DIR="${DIR}/modules"
 
 # compress: {
 # sequences: true,
@@ -18,12 +19,12 @@ UGLIFYJS_OPTS=""
 lsc -cb init.ls
 
 # create bundle
-BUNDLE="init.min.js"
-echo "" > ${BUNDLE}
+BUNDLE="app.min.js"
+echo > ${BUNDLE}
 
 # Register modules via `Modules.addCached()` method
 # see: http://forum.espruino.com/comments/12899741/
 MODULE_NAME="FlashEEPROM"
-MODULE_STR=$(uglifyjs ${UGLIFYJS_OPTS} -- ${MODULE_NAME}.js | sed 's/\"/\\\"/g' | sed "s/\n//g")
+MODULE_STR=$(uglifyjs ${UGLIFYJS_OPTS} -- ${MODULES_DIR}/${MODULE_NAME}.js | sed 's/\"/\\\"/g' | sed "s/\n//g")
 echo "Modules.addCached(\"${MODULE_NAME}\", \"${MODULE_STR}\");" >> ${BUNDLE}
 uglifyjs ${UGLIFYJS_OPTS} -- init.js >> ${BUNDLE}
