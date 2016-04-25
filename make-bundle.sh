f9b7b5458e74b51e7e49fa97b2b648fc8fdefbae
Cerem Cem ASLAN <ceremcem@ceremcem.net>
2016-04-25 15:20:49 +0300

    bundler automatically includes required modules

--- make-bundle.sh
+++ make-bundle.sh
@@ -14,17 +14,27 @@ MODULES_DIR="${DIR}/modules"
 # if_return: true,
 # join_vars: true,
 # drop_console: true
-UGLIFYJS_OPTS=""
+UGLIFYJS_OPTS="-c -m"
 
 lsc -cb init.ls
 
 # create bundle
 BUNDLE="app.min.js"
 echo > ${BUNDLE}
+uglifyjs ${UGLIFYJS_OPTS} -- init.js >> ${BUNDLE}
+
+MODULES=$(cat ${BUNDLE} | grep -o 'require("[a-ZA-Z0-9]\+")' | grep -o '".*"' | sed 's/"//g')
+#echo "INFO: Required modules: ${MODULES}"
 
 # Register modules via `Modules.addCached()` method
 # see: http://forum.espruino.com/comments/12899741/
-MODULE_NAME="FlashEEPROM"
-MODULE_STR=$(uglifyjs ${UGLIFYJS_OPTS} -- ${MODULES_DIR}/${MODULE_NAME}.js | sed 's/\"/\\\"/g' | sed "s/\n//g")
-echo "Modules.addCached(\"${MODULE_NAME}\", \"${MODULE_STR}\");" >> ${BUNDLE}
-uglifyjs ${UGLIFYJS_OPTS} -- init.js >> ${BUNDLE}
+#MODULES="FlashEEPROM DS18B20"
+for MODULE_NAME in $MODULES; do
+    if [[ -f "${MODULES_DIR}/${MODULE_NAME}.js" ]]; then 
+        echo "INFO: * Adding module: ${MODULE_NAME}"
+        MODULE_STR=$(uglifyjs ${UGLIFYJS_OPTS} -- ${MODULES_DIR}/${MODULE_NAME}.js | sed 's/\"/\\\"/g' | sed "s/\n//g")
+        echo "Modules.addCached(\"${MODULE_NAME}\", \"${MODULE_STR}\");" >> ${BUNDLE}
+    else
+        echo "INFO: ### Module ${MODULE_NAME} is embedded??"
+    fi
+done 
