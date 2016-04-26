9c2f66929c524a389970021682934a6e6f7ace28
Cerem Cem ASLAN <ceremcem@ceremcem.net>
2016-04-27 00:14:43 +0300

    subdependency tracking is fixed

--- make-bundle.sh
+++ make-bundle.sh
@@ -23,41 +23,20 @@ lsc -cb init.ls
 BUNDLE="app.min.js"
 echo > ${BUNDLE}
 BUNDLE_CONTENT=$(uglifyjs ${UGLIFYJS_OPTS} -- init.js)
-req=$(echo ${BUNDLE_CONTENT} | grep -o 'require([\\\"a-zA-Z0-9]\+)' | sed 's|\\||g' | grep -o '".*"' | sed 's/"//g' | tr '\n' ' ')
-read -a MODULES <<<$req
 
+# Get initial modules that are required in "init.ls"
+req=$(echo ${BUNDLE_CONTENT} | grep -o 'require([\\\"a-zA-Z0-9]\+)' | sed 's|\\||g' | grep -o '".*"' | sed 's/"//g' | tr '\n' ' ')
+MODULES=(`echo ${req}`)
+
+# debug
+for i in "${MODULES[@]}"; do echo "modules in init: $i"; done
 
 # Register modules via `Modules.addCached()` method
 # see: http://forum.espruino.com/comments/12899741/
 i=0
 while true; do
     MODULE_NAME="${MODULES[$i]}"
-    
-    req=$(cat ${BUNDLE} | grep -o 'require([\\\"a-zA-Z0-9]\+)' | sed 's|\\||g' | grep -o '".*"' | sed 's/"//g' | tr '\n' ' ')
-    #echo "New dependencies: $req"
-    SUB_DEPS=(`echo ${req}`)
-    # add modules if not found in $MODULES
-    for j in "${SUB_DEPS[@]}"; do
-        #echo "DEBUG: looking for $j"
-        add="yes"
-        for k in "${MODULES[@]}"; do 
-            #echo "DEBUG: ......modules so far: $k"
-            if [[ "$j" == "$k" ]]; then 
-                add="no"
-                #echo "DEBUG: not adding module: $j"
-                break
-            fi
-        done
-        if [[ "$add" == "yes" ]]; then 
-            echo "... Adding sub dependency: $j"
-            MODULES+=( "$j" )
-        fi
-    done
-    
-    
-
-    
-    if [[ -f "${MODULES_DIR}/${MODULE_NAME}.js" ]]; then 
+    if [[ -f "${MODULES_DIR}/${MODULE_NAME}.js" ]]; then
         echo "INFO: * Adding module: ${MODULE_NAME}"
         MODULE_STR=$(uglifyjs ${UGLIFYJS_OPTS} -- ${MODULES_DIR}/${MODULE_NAME}.js | sed 's/\"/\\\"/g' | sed "s/\n//g")
         echo "Modules.addCached(\"${MODULE_NAME}\", \"${MODULE_STR}\");" >> ${BUNDLE}
@@ -68,6 +47,32 @@ while true; do
     else
         echo "INFO: ?? Module '${MODULE_NAME}' is embedded??"
     fi
+
+
+    # Add if any subdependency is required
+    # ----------------------------------------
+    req=$(cat ${BUNDLE} | grep -o 'require([\\\"a-zA-Z0-9]\+)' | sed 's|\\||g' | grep -o '".*"' | sed 's/"//g' | tr '\n' ' ')
+    #echo "New dependencies: $req"
+    SUB_DEPS=(`echo ${req}`)
+    # add modules if not found in $MODULES
+    for j in "${SUB_DEPS[@]}"; do
+        #echo "DEBUG: looking for $j"
+        add="yes"
+        for k in "${MODULES[@]}"; do
+            #echo "DEBUG: ......modules so far: $k"
+            if [[ "$j" == "$k" ]]; then
+                add="no"
+                #echo "DEBUG: not adding module: $j"
+                break
+            fi
+        done
+        if [[ "$add" == "yes" ]]; then
+            echo "... Adding sub dependency: $j"
+            MODULES+=( "$j" )
+        fi
+    done
+
+
     i=$((i+1))
     num_of_modules=${#MODULES[@]}
     if (("$i" >= "$num_of_modules")) ; then 
