78d580b44ef7f55e6bce9de3551f669db5d30b5c
Cerem Cem ASLAN <ceremcem@ceremcem.net>
2016-05-13 16:00:13 +0300

    firmware is now bundled with toolset

--- aea-espruino-load-firmware.sh
+++ aea-espruino-load-firmware.sh
@@ -1,20 +1,29 @@
 #!/bin/bash
 
-CURR_PWD=$(pwd)
-
 PORT='/dev/ttyUSB0'
 if [[ "$1" != "" ]]; then
     PORT="$1"
 fi
 
-MAKEFILE_FLASH_SIZE="4m"
+ESP_FIRMWARE_DIR="/home/ceremcem/embedded/Espruino/build/espruino_1v85.131_esp8266"
+
+# See ${ESP_FIRMWARE_DIR}/README_flash.md
+BAUD=115200
+ESP_512K_CMD="esptool.py --port ${PORT} --baud ${BAUD} write_flash \
+  --flash_freq 40m --flash_mode qio --flash_size 4m \
+  0x0000 boot_v1.4(b1).bin 0x1000 espruino_esp8266_user1.bin 0x7E000 blank.bin"
+
+ESP_4MB_CMD="esptool.py --port ${PORT} --baud ${BAUD} write_flash \
+  --flash_freq 80m --flash_mode qio --flash_size 32m \
+  0x0000 boot_v1.4(b1).bin 0x1000 espruino_esp8266_user1.bin 0x37E000 blank.bin"
+
+
+ESP_ESPRUINO_CMD=${ESP_512K_CMD}
 FLASH_SIZE=$(config-md -i config.md -k "hardware.flash size")
 echo "FLASH size: $FLASH_SIZE"
 if [[ "$FLASH_SIZE" == "4MB" ]]; then
-    MAKEFILE_FLASH_SIZE="32m"
+    ESP_ESPRUINO_CMD=${ESP_4MB_CMD}
 fi
-
-ESPTOOL_OPTS="PORT=$PORT FLASH_SIZE=$MAKEFILE_FLASH_SIZE"
 
 
 wait_port_unplugged () {
@@ -46,30 +55,33 @@ wait-power-toggle() {
 }
 
 
-EMBEDDED="${HOME}/embedded"
-cd $EMBEDDED
 
 echo "-------------------------------------------"
 echo "ENTER the programming mode, toggle power..."
 echo "-------------------------------------------"
 read -p ''
 
+CURR_DIR=$(pwd)
+cd ${ESP_FIRMWARE_DIR}
 
-until make erase-flash ${ESPTOOL_OPTS}
+#until esptool.py --port ${PORT} erase_flash
+#do
+#    wait-power-toggle
+#done
+#
+#wait-power-toggle
+
+until ${ESP_ESPRUINO_CMD}
 do
     wait-power-toggle
 done
 
-wait-power-toggle
-
-until make flash-espruino-esp8266-firmware ${ESPTOOL_OPTS}
-do
-    wait-power-toggle
-done
+cd ${CURR_DIR}
 
 echo "----------------------------------------"
 echo "EXIT PROGRAMMING MODE, Toggle Power...."
 echo "----------------------------------------"
 read -p ''
 
-cd $CURR_PWD
+
+
